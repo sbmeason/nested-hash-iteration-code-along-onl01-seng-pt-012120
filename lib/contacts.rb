91249d1fe879e3046da9dc1eed411ed74d4a35e7
Samantha Meason <sbmeason@gmail.com>
2020-02-03 20:29:53 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -30,7 +30,7 @@ contacts = {
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry 
+      binding.pry
     end
   end
 end
